1bb663353e267df9719906b70a8987ece9e65bf2
Luo-Yun-Rong <b06901073@ntu.edu.tw>
2021-12-15 21:53:47 +0800

    Add PA2 grading script

--- lsv_fall_2021/admin/grade-submissions.sh
+++ lsv_fall_2021/admin/grade-submissions.sh
@@ -1,66 +1,64 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <ALL|branch-name>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <ALL|branch-name> <mac|linux>"
     echo "ALL: grade ALL students"
     echo "branch-name: the branch to grade, e.g., d04943019"
     exit 1
 fi
 
-pa_cmd="lsv_print_msfc"
-pa_dir="lsv_fall_2021/pa1"
+time_limit="60s"
+pa_cmd="lsv_or_bidec"
+checker_cmd="lsv_pa2_checker"
+pa_dir="lsv_fall_2021/pa2"
 ref_dir="${pa_dir}/ref"
 out_dir="${pa_dir}/out"
-diff_dir="${pa_dir}/diff"
+refprog="./abc.$2"
 bench_dir="benchmarks"
-bench_list=( $(find -L benchmarks/arithmetic/ -type f -name '*.aig') 
-             $(find -L benchmarks/random_control/ -type f -name '*.aig') )
+bench_list=("${bench_dir}/random_control/arbiter.aig" 
+            "${bench_dir}/random_control/cavlc.aig"
+            "${bench_dir}/random_control/ctrl.aig"
+            "${bench_dir}/random_control/dec.aig"
+            "${bench_dir}/random_control/i2c.aig"
+            "${bench_dir}/random_control/int2float.aig"
+            "${bench_dir}/random_control/priority.aig"
+            "${bench_dir}/random_control/router.aig"
+            "${bench_dir}/arithmetic/bar.aig")
 students=( $(cut -d, -f1 < lsv_fall_2021/admin/participants-id.csv | tail -n +3) )
+
 
 grade_one_branch () {
     student="$1"
     result="${pa_dir}/${student}.csv"
     git switch "${student}"
-    for extdir in src/ext*; do
-        rm ${extdir}/*.o ${extdir}/*.d
-    done
-    rm src/base/abci/*.o src/base/abci/*.d
+    rm src/ext-lsv/*.o src/ext-lsv/*.d \
+        src/sat/bsat/*.o src/sat/bsat/*.d \
+        src/sat/cnf/*.o src/sat/cnf/*.d \
+        src/proof/fra/*.o src/proof/fra/*.d \
+        src/base/abci/*.o src/base/abci/*.d
     make -j8
     echo "Benchmark,Result" > "${result}"
-    rm -rf "${out_dir}" "${diff_dir}"
+    rm -rf "${out_dir}" "${ref_dir}"
     mkdir -p "${out_dir}"
-    mkdir -p "${diff_dir}"
-
-    declare -i blif_eq=0 
-    for blif in ${pa_dir}/*.blif; do 
-        echo "[INFO] Testing with ${blif} ..."
-        ./abc -c "dsec -n ${ref_dir}/sa4_ref.blif ${blif}" > "${out_dir}/sa4.txt" 
-        match=$(grep -o -c "Networks are equivalent" ${out_dir}/sa4.txt )  
-        if [ ${match} -eq 1 ]; then
-                ((++blif_eq))
-            echo "${blif},Pass" >> "${result}"
-        else
-            echo "${blif},Fail" >> "${result}"
-        fi 
-    done
+    mkdir -p "${ref_dir}"
     declare -i correct=0
     for bench in "${bench_list[@]}"; do
         echo "[INFO] Testing with ${bench} ..."
         bch_name=$(echo "${bench}" | awk -F "/" '{print $(NF)}' | sed -e 's/.aig$//')
-        ./abc -c "${pa_cmd}" "${bench}" > "${out_dir}/${bch_name}.txt"
-        diff -uwiB "${ref_dir}/${bch_name}.txt" "${out_dir}/${bch_name}.txt" > "${diff_dir}/${bch_name}.txt"
-        if [ "$?" -eq 0 ]; then
-            ((++correct))
+        timeout "${time_limit}" ./abc -c "${pa_cmd}" "${bench}" > "${out_dir}/${bch_name}.txt"
+        ${refprog} -c "${checker_cmd} ${out_dir}/${bch_name}.txt" "${bench}" > "${ref_dir}/${bch_name}.txt" 
+        match=$(grep -o -c "Pass!" ${ref_dir}/${bch_name}.txt )  
+        if [ ${match} -eq 1 ]; then
+                ((++correct))
             echo "${bench},Pass" >> "${result}"
         else
             echo "${bench},Fail" >> "${result}"
-        fi
+        fi 
     done
     local __return_var="$2"
-    local __point=$(echo "${correct}*4+${blif_eq}*5" | bc)
+    local __point=$(echo "${correct}*11+1" | bc)
     eval "${__return_var}"="${__point}"
-    echo "[INFO] Correct BLIF  in part 1: ${blif_eq}"
-    echo "[INFO] Correct cases in part 3: ${correct}"
+    echo "[INFO] Correct cases: ${correct}"
     echo "[INFO] Total points: ${__point}"
     echo "Score,${__point}" >> "${result}"
 }
@@ -72,7 +70,7 @@ if [ "$1" = "ALL" ]; then
         grade_one_branch "${student}" point
         student_points+=("${point}")
         git add -f "${pa_dir}/${student}.csv"
-        git add -f "${out_dir}" "${diff_dir}"
+        git add -f "${out_dir}" "${ref_dir}"
         git commit -m "Grade branch ${student}"
         git push
         git stash push -m "Garbage from ${student}"
@@ -88,5 +86,5 @@ if [ "$1" = "ALL" ]; then
     git push
 else
     echo "[INFO] Grading branch $1 ..."
-    grade_one_branch "$1" point
+    grade_one_branch "$1" point 
 fi
